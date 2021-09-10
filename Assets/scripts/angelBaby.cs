38e41c415730b505d57cf50544ce893219d6b539
Maria Roxas <mer671@nyu.edu>
2021-09-10 12:14:41 -0400

    coded one angel to randomly spawn at the top!

---
+++ Assets/scripts/angelBaby.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class angelBaby : MonoBehaviour
+{
+
+    public bool babyACTIVE = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+// to check if angelBaby is on the screen
+    void OnBecameVisible(){ // if it is
+        Debug.Log("angelspawn"); // lmk !
+        babyACTIVE = true;
+    }
+}
