69c941f672b124c658aa2c7a19252211d1c91412
Maria Roxas <mer671@nyu.edu>
2021-09-10 00:24:58 -0400

    added comments , press 5 to start the game , basic shooting mechanic

---
+++ Assets/scripts/playerShoot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerShoot : MonoBehaviour
+{
+
+// PUBLIC
+    public GameObject beam; // the player's ghost bullet >:)
+    public GameObject baker; // the player :)
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        baker.GetComponent<playerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(baker.GetComponent<playerMovement>().SPOOKBREAD){
+            // shoot up
+            GameObject gbeam = (GameObject)Instantiate (beam, transform.position, Quaternion.identity);
+
+            gbeam.GetComponent<bulletBehavior>().ySpeed = 1f;
+        }
+    }
+}
